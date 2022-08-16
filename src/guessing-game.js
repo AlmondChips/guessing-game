f7352e7ae4e55431e67ccf1fb816c9523637bc96
AlmondChips <chips6t@gmail.com>
2022-08-16 22:41:19 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -7,31 +7,26 @@ class GuessingGame {
 	}
 
     setRange(min, max) {
-		if((max%2) === 0){
 			for(let i = min; i <= max; i++){
 				this.array.push(i)
 			}
-		} else{
-			for(let i = min+1; i <= max; i++){
-				this.array.push(i)
-			}
-		}
+
 		
 		this.right = this.array.length - 1;
     }
 
     guess() {
-		this.mid = Math.round((this.right-this.left)/2) + this.left;
+		this.mid = Math.round((this.right+this.left)/2);
 		return this.array[this.mid];
 		
     }
 
     lower() {
-		this.right = this.mid - 1;
+		this.right = this.mid;
     }
 
     greater() {
-		this.left = this.mid + 1;
+		this.left = this.mid;
     }
 }
 
